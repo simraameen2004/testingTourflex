4e35f92d16cd88236ebdcbf7302f626e9f9369c2
simraameen2004 <shimra18.01@gmail.com>
2026-05-14 18:16:50 +0530

    Delete IT25100815/src/main directory

--- IT25100815/src/main/java/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tourflex.repository;
-
-import com.tourflex.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-
-    User findByEmail(String email);
-}
